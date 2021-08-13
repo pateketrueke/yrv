3c2e380d4bdca10703f3ed61cb08982649cd185a
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-08-12 22:44:35 -0500

    chore: fixes some testing nuances

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import Router from './lib/Router.svelte';
-
-import { hashchangeEnable } from './lib/utils';
-
 export { default as Router } from './lib/Router.svelte';
 export { default as Route } from './lib/Route.svelte';
 export { default as Link } from './lib/Link.svelte';
 
 export { navigateTo, router } from './lib/utils';
-
-Object.defineProperty(Router, 'hashchange', {
-  set: value => hashchangeEnable(value),
-  get: () => hashchangeEnable(),
-  configurable: false,
-  enumerable: false,
-});
